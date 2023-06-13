ea8a09e48474af4ec44456422cddf5127930afe6
Ian Roberts <ijr2399@gmail.com>
2023-06-13 11:07:13 -0500

    Got websockets working

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+import {useState, useEffect} from 'react';
 import './App.css';
+import {socket} from './socket';
 
 function App() {
+  const [connected, setConnected] = useState(false);
+
+  useEffect(() => {
+    function onConnect() {
+      setConnected(true);
+    }
+
+    function onDisconnect() {
+      setConnected(false);
+    }
+
+    socket.on('connect', onConnect);
+    socket.on('disconnect', onDisconnect)
+    
+    return () => {
+      socket.off('connect', onConnect);
+      socket.off('disconnect' ,onDisconnect);
+    }
+  
+  })
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    if (connected) {
+      socket.disconnect();
+    } else {
+      socket.connect();
+    }
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleClick}>{connected ? "Disconnect" : "Connect" }</button>
     </div>
   );
 }
