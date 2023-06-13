61829012522ce2cbaded9cdd5393840ecb1455ee
Ian Roberts <ijr2399@gmail.com>
2023-06-13 11:07:46 -0500

    Updated server functions

--- server/app.js
+++ server/app.js
@@ -1,10 +1,22 @@
 const express = require('express');
-const server = express();
+const {Server} = require('socket.io');
+const { createServer } = require("http");
 const port = 8080;
 
+const app = express();
+const expressServer= createServer(app)
+const socketServer = new Server(expressServer, {
+  cors: {
+    origin: "http://localhost:3000"
+  }
+});
 
-server.use(express.json());
-server.use((req, res, next) => {
+let players = 0;
+
+
+
+app.use(express.json());
+app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
@@ -14,9 +26,30 @@ server.use((req, res, next) => {
   next();
 });
 
+socketServer.on('connection', (socket) => {
+
+  console.log("A player connected");
+  if (players >= 2) {
+    socket.emit("error", "Too many players on currently.");
+    socket.disconnect(true);
+
+  } else {
+    players++;
+    socket.join('game');
+    let gameOver = false;
+    while (players === 2 && !gameOver) {
+      
+    }
+  }
+
+  socket.on('disconnect', (socket) => {
+    console.log('A player disconnected')
+    players--;
+  });
+})
 
 
-server.get('/', (req, res) => {
+app.get('/', (req, res) => {
   //TODO
 })
 
@@ -24,4 +57,4 @@ server.get('/', (req, res) => {
 
 
 
-server.listen(port, () => console.log(`Server listening on port ${port}`));
+expressServer.listen(port, () => console.log(`Express Server listening on port ${port}`));
